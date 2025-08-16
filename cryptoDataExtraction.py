77566fa346d8d5adaa31a9bce297d9f47e37de8f
PigX12 <jtao2@andrew.cmu.edu>
2025-08-15 23:19:21 -0400

    Update cryptoDataExtraction.py

--- cryptoDataExtraction.py
+++ cryptoDataExtraction.py
@@ -7,4 +7,16 @@ address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
 currentWeiBalance = w3.eth.get_balance(address)
 currentEtherBalance = Web3.from_wei(currentWeiBalance, "ether")
 
-print(f'Current balance is {currentEtherBalance}')
+def scoreCounter(walletAge, transactionCount, scamInteraction, noScamInteraction):
+    score = 0
+    if 1 <= walletAge <= 3:
+        score += 10
+    elif walletAge > 3:
+        score += 20
+    elif transactionCount > 100:
+        score += 20
+    elif scamInteraction == True:
+        score -= 50
+    elif noScamInteraction == True:
+        score += 30
+    return score
