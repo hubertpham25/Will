16cdbed0f8166bed732e36b7f8adb0196b2cdcdb
PigX12 <jtao2@andrew.cmu.edu>
2025-08-15 23:43:53 -0400

    Update cryptoDataExtraction.py

--- cryptoDataExtraction.py
+++ cryptoDataExtraction.py
@@ -1,22 +1,20 @@
 from web3 import Web3
+from dotenv import load_dotenv
+import os
+
+load_dotenv
 
 alchemyNodeURL = "https://eth-mainnet.g.alchemy.com/v2/jeCvmjj_CZDDvzJxXxyq_"
+etherscankey = os.getenv("ETHERSCAN_API")
 w3 = Web3(Web3.HTTPProvider(alchemyNodeURL))
+print(etherscankey)
 
 address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
-currentWeiBalance = w3.eth.get_balance(address)
-currentEtherBalance = Web3.from_wei(currentWeiBalance, "ether")
 
-def scoreCounter(walletAge, transactionCount, scamInteraction, noScamInteraction):
-    score = 0
-    if 1 <= walletAge <= 3:
-        score += 10
-    elif walletAge > 3:
-        score += 20
-    elif transactionCount > 100:
-        score += 20
-    elif scamInteraction == True:
-        score -= 50
-    elif noScamInteraction == True:
-        score += 30
-    return score
+def getWalletBalance(address):
+    currentWeiBalance = w3.eth.get_balance(address)
+    currentEtherBalance = Web3.from_wei(currentWeiBalance, "ether")
+    return currentEtherBalance
+
+def getTransactions(address):
+    return None
