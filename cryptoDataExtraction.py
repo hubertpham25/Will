107bb84da1e28828163686957df07313d1b26d91
PigX12 <jtao2@andrew.cmu.edu>
2025-08-16 00:11:08 -0400

    Update cryptoDataExtraction.py

--- cryptoDataExtraction.py
+++ cryptoDataExtraction.py
@@ -1,11 +1,12 @@
 from web3 import Web3
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
 alchemyNodeURL = "https://eth-mainnet.g.alchemy.com/v2/jeCvmjj_CZDDvzJxXxyq_"
-etherscankey = os.getenv("ETHERSCAN_API")
+etherscanKey = os.getenv("ETHERSCAN_API")
 w3 = Web3(Web3.HTTPProvider(alchemyNodeURL))
 
 address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
@@ -16,4 +17,11 @@ def getWalletBalance(address):
     return currentEtherBalance
 
 def getTransactions(address):
-    return 
+    url = f"https://api.etherscan.io/api?module=account&action=txlist&address={address}&sort=asc&apikey={etherscanKey}"
+    response = requests.get(url).json()
+    
+    totalTransactions = response["result"] #list of dictionaries for each transactions
+    
+    numOfTransactions = len(totalTransactions) #number of transactions on address
+    
+    
