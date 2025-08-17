ef388351048495bf4db542dd21ee6b1431a21f4c
hubertpham25 <phamhubert820@gmail.com>
2025-08-17 00:52:49 -0400

    ...

--- data_processing.py
+++ data_processing.py
@@ -11,16 +11,28 @@ def calc_rep(urlPath):
         # checks if the current time and the timestamp of the address is less than seconds in a day
         if time.time() - timestamp < sec_in_day:
             return jsonify({ 
-                            "wallet address: ": wallet_address, 
-                            "wallet reputation score: ": cached_score 
+                            "Wallet Address": wallet_address, 
+                            "Wallet Reputation Score": cached_score,
+                            "Safe Address": evaluation
                         })
     
     # if not, it will get a new score for the day
     new_score = calc_new_rep(wallet_address)
+    max_score = 200
+    if new_score/max_score < 0.2:
+        evaluation = "Not reputable. User does not have enough transaction history."
+    
+    elif new_score/max_score < 0.6:
+        evaluation = "Slightly reputable. User has enough transactions. However, still proceed with caution."
+    
+    else:
+        evaluation = "Reputable. User has enough transactions. Still proceed with caution as always."
+
     wallet_address_cache[wallet_address] = (new_score, time.time())
     return jsonify({
                     "wallet address": wallet_address,
-                    "wallet reputation score": new_score
+                    "wallet reputation score": new_score,
+                    "Safe Address": evaluation
                    })
 
 def extract_wallet_address(urlPath):
@@ -37,28 +49,32 @@ def calc_new_rep(address):
     num_of_transaction = cryptoDB["Amounts of Transaction"]
     wallet_age = cryptoDB["Wallet Age"]
     rep_score = get_rep_score(num_of_transaction, address)
+    scam_status = cryptoDB["Scam Status"]
+
+    if scam_status == True:
+        rep_score *= 0.125
 
 def get_rep_score(num_of_transaction, address):
-    max_score = 300
+    max_score = 200
     cached_score, timestamp = wallet_address_cache[address]
     rep_score = 0
     if cached_score != 0:
         rep_score = cached_score
     
-    if num_of_transaction <= 20:
-        rep_score += num_of_transaction*0.05
+    if num_of_transaction > 0:
+        rep_score += min(num_of_transaction, 20)*0.05
     
-    elif 20 < num_of_transaction <= 100:
-        rep_score += num_of_transaction*0.07
+    elif num_of_transaction >20 :
+        rep_score += min(num_of_transaction, 100)*0.07
 
     elif 100 < num_of_transaction <= 500:
-        rep_score += num_of_transaction*0.1
+        rep_score += min(num_of_transaction, 500)*0.1
     
     elif num_of_transaction > 500 and rep_score < max_score:
         rep_score += num_of_transaction*0.12
     
     else: 
-        rep_score = 300
-        
+        rep_score = 200
+
     return rep_score
 
